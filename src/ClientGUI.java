875735a70b46debf36712c44407606a2a69155bf
Mark Berney <berneydwmark@gmail.com>
2019-08-05 13:52:20 +1200

    ChatPanel and ChatToolbars added
    Chat toolbars include send message field and title toolbar

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,13 +1,23 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class ClientGUI {
+    private JButton sendMessageButton;
     private JFrame frame;
+    private JLabel appTitle;
+    private JPanel chatPanel;
+    private JPanel chatToolbarPanel;
     private JPanel connectionPanel;
     private JPanel parentPanel;
+    private JPanel sendMessageToolbarPanel;
+    private JTextField sendMessageField;
 
     public ClientGUI() {
         GUIInit();
+        setChatPanel();
+        setChatToolBar();
+        setSendMessageToolbar();
         setConnectionPanel();
         frame.setVisible(true);
     }
@@ -23,9 +33,39 @@ public class ClientGUI {
         frame.add(parentPanel);
     }
 
+    private void setChatPanel() {
+        chatPanel = new JPanel();
+        chatPanel.setLayout(new BorderLayout());
+        chatPanel.setPreferredSize(new Dimension(600, 600));
+        chatPanel.setBackground(Color.YELLOW);
+        parentPanel.add(chatPanel, BorderLayout.WEST);
+    }
+    
+    private void setChatToolBar() {
+        chatToolbarPanel = new JPanel();
+        chatToolbarPanel.setBackground(Color.RED);
+        chatPanel.add(chatToolbarPanel, BorderLayout.NORTH);
+
+        appTitle = new JLabel("Chat Room");
+        chatToolbarPanel.add(appTitle);
+    }
+
+    private void setSendMessageToolbar() {
+        sendMessageToolbarPanel = new JPanel();
+        chatPanel.add(sendMessageToolbarPanel, BorderLayout.SOUTH);
+
+        sendMessageField = new JTextField();
+        sendMessageField.setPreferredSize(new Dimension(500, sendMessageField.getPreferredSize().height));
+        sendMessageToolbarPanel.add(sendMessageField);
+
+        sendMessageButton = new JButton("SEND");
+        sendMessageToolbarPanel.add(sendMessageButton);
+    }
+
     private void setConnectionPanel() {
         connectionPanel = new JPanel();
         connectionPanel.setPreferredSize(new Dimension(300, 600));
+        connectionPanel.setBackground(Color.GREEN);
         parentPanel.add(connectionPanel, BorderLayout.EAST);
     }
 
