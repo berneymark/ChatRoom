acf20fd4d9e950388306c2d34e7aceb0996f6704
Mark Berney <berneydwmark@gmail.com>
2019-08-05 13:00:47 +1200

    Basic JFrame completed

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,5 +1,15 @@
+import javax.swing.*;
+
 public class ClientGUI {
     public ClientGUI() {
-        
+        JFrame frame = new JFrame("Chat Room");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1200, 600);
+        frame.getContentPane();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ClientGUI client = new ClientGUI();
     }
 }
