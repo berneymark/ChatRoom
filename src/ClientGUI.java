d6f58500e01409e6599b948a5e1783394d3ef7c2
Mark Berney <berneydwmark@gmail.com>
2019-08-05 13:12:54 +1200

    Add connectionPanel to GUI

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -1,12 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class ClientGUI {
+    private JFrame frame;
+    private JPanel connectionPanel;
+    private JPanel parentPanel;
+
     public ClientGUI() {
-        JFrame frame = new JFrame("Chat Room");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1200, 600);
-        frame.getContentPane();
+        GUIInit();
+        setConnectionPanel();
         frame.setVisible(true);
+    }
+
+    private void GUIInit() {
+        frame = new JFrame("Chat Room");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(900, 600);
+        frame.getContentPane();
+
+        parentPanel = new JPanel();
+        parentPanel.setLayout(new BorderLayout());
+        frame.add(parentPanel);
+    }
+
+    private void setConnectionPanel() {
+        connectionPanel = new JPanel();
+        connectionPanel.setPreferredSize(new Dimension(300, 600));
+        parentPanel.add(connectionPanel, BorderLayout.EAST);
     }
 
     public static void main(String[] args) {
