c1566b1e22d2e8f5b17dfc138757959b3fcafba2
Mark Berney <berneydwmark@gmail.com>
2019-08-13 20:49:14 +1200

    Server GUI + shutdown

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
@@ -18,6 +22,8 @@ public class ChatServer {
     }
 
     public void startServer() {
+        ServerGUI gui = new ServerGUI();
+
         try (ServerSocket serverSocket  = new ServerSocket(HOST_PORT)) {
             System.out.println("Server started at " +
                     InetAddress.getLocalHost() +
@@ -85,11 +91,55 @@ public class ChatServer {
     }
 
     public void requestStop() {
+        System.out.println("Server shutting down.");
         stopRequested = true;
+        System.exit(0);
     }
 
     public static void main(String[] args) {
         ChatServer cs = new ChatServer();
         cs.startServer();
+    }
+
+    private class ServerGUI extends JFrame {
+        private JPanel parentPanel;
+        private JButton stopServer;
+        private JLabel serverLabel;
+
+        public ServerGUI() {
+            buildGUI();
+            additions();
+            this.setVisible(true);
+        }
+
+        public void buildGUI() {
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setSize(300, 100);
+            this.setTitle("Chat Server");
+            this.getContentPane();
+
+            Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+            this.setLocation(
+                    dim.width / 2 - this.getSize().width / 2,
+                    dim.height / 2 - this.getSize().height / 2
+            );
+
+            parentPanel = new JPanel();
+            this.add(parentPanel);
+        }
+
+        public void additions() {
+            serverLabel = new JLabel("Server");
+            parentPanel.add(serverLabel);
+
+            stopServer = new JButton("Shutdown");
+            stopServer.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    requestStop();
+                }
+            });
+            parentPanel.add(stopServer);
+        }
     }
 }
